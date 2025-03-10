b8dbed203e882f4a7e3a2d885341979c23523542
Zulikath <69781787+zulikath@users.noreply.github.com>
2025-03-09 19:48:37 -0500

    fix missing overlapping diagonals, add tests needed to check for that

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -90,30 +90,38 @@ fn search_diagonal(band: &[Vec<char>]) -> i32 {
     let len = band.get(0).unwrap_or(&vec![]).len();
 
     for column in 0..len {        
-        if ( 
-            // NE-SW
+        if  // NE-SW: first index ascending, second ascending
             band.get(0).and_then(|row| row.get(column+0)) == X &&
             band.get(1).and_then(|row| row.get(column+1)) == M &&
             band.get(2).and_then(|row| row.get(column+2)) == A &&
             band.get(3).and_then(|row| row.get(column+3)) == S
-            ) || (
-            // NW-SE
+        {
+            count += 1;
+        }
+
+        if  // NW-SE: first index ascending, second descending.
             band.get(0).and_then(|row| row.get(column+3)) == X &&
             band.get(1).and_then(|row| row.get(column+2)) == M &&
             band.get(2).and_then(|row| row.get(column+1)) == A &&
             band.get(3).and_then(|row| row.get(column+0)) == S
-            ) || (
-            // SE-NW
+        {
+            count += 1;
+        }
+
+        if  // SE-NW: first index descending, second ascending
             band.get(3).and_then(|row| row.get(column+0)) == X &&
             band.get(2).and_then(|row| row.get(column+1)) == M &&
             band.get(1).and_then(|row| row.get(column+2)) == A &&
             band.get(0).and_then(|row| row.get(column+3)) == S
-            ) || (
-            // SW-NE
+        {
+            count += 1;
+        }
+
+        if  // SW-NE: first index descending, second ascending
             band.get(3).and_then(|row| row.get(column+3)) == X &&
             band.get(2).and_then(|row| row.get(column+2)) == M &&
             band.get(1).and_then(|row| row.get(column+1)) == A &&
-            band.get(0).and_then(|row| row.get(column+0)) == S)
+            band.get(0).and_then(|row| row.get(column+0)) == S
         {
             count += 1;
         }
@@ -122,4 +130,83 @@ fn search_diagonal(band: &[Vec<char>]) -> i32 {
     count
 }
 
+#[cfg(test)]
+mod tests {
+    use super::*;
 
+    #[test]
+    fn diagonal() {
+        let test = |grid: Vec<Vec<char>>| grid.windows(4).map(search_diagonal).sum::<i32>();
+
+        let grid_nwse = vec![
+            vec!['X', ' ', ' ', ' '],
+            vec![' ', 'M', ' ', ' '],
+            vec![' ', ' ', 'A', ' '],
+            vec![' ', ' ', ' ', 'S'],
+        ];
+
+        let grid_nesw = vec![
+            vec![' ', ' ', ' ', 'X'],
+            vec![' ', ' ', 'M', ' '],
+            vec![' ', 'A', ' ', ' '],
+            vec!['S', ' ', ' ', ' '],
+        ];
+
+        let grid_swne = vec![
+            vec![' ', ' ', ' ', 'S'],
+            vec![' ', ' ', 'A', ' '],
+            vec![' ', 'M', ' ', ' '],
+            vec!['X', ' ', ' ', ' '],
+        ];
+
+        let grid_senw = vec![
+            vec!['S', ' ', ' ', ' '],
+            vec![' ', 'A', ' ', ' '],
+            vec![' ', ' ', 'M', ' '],
+            vec![' ', ' ', ' ', 'X'],
+        ];
+
+        let grid_overlapping = vec![
+            vec!['X', ' ', ' ', 'X'],
+            vec![' ', 'M', 'M', ' '],
+            vec![' ', 'A', 'A', ' '],
+            vec!['S', ' ', ' ', 'S'],
+        ];
+
+        assert_eq!(test(grid_nwse), 1i32);
+        assert_eq!(test(grid_nesw), 1i32);
+        assert_eq!(test(grid_swne), 1i32);
+        assert_eq!(test(grid_senw), 1i32);
+        assert_eq!(test(grid_overlapping), 2i32);
+
+    }
+
+    #[test]
+    fn vertical() {
+        let test = |grid: Vec<Vec<char>>| grid.windows(4).map(search_vertical).sum::<i32>();
+
+        let grid = vec![
+            vec!['X', 'M', 'A', 'S'],
+            vec!['M', 'A', 'M', 'A'],
+            vec!['A', 'M', 'A', 'M'],
+            vec!['S', 'M', 'A', 'X'],
+        ];
+
+        assert_eq!(test(grid), 2i32);
+
+    }
+
+    #[test]
+    fn horizontal() {
+        let test = |grid: Vec<Vec<char>>| grid.iter().map(search_horizontal).sum::<i32>();
+
+        let grid = vec![
+            vec!['X', 'M', 'A', 'S'],
+            vec!['S', 'A', 'M', 'X'],
+            vec!['X', 'M', 'A', 'S'],
+            vec!['X', 'M', 'A', 'S'],
+        ];
+
+        assert_eq!(test(grid), 4i32);
+    }
+}
